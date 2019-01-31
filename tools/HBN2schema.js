93fd15a649ec9bd7a5fb860bbb581fb9fcff92fa
Sanu Ann <sanuann@mit.edu>
2019-01-31 17:14:25 -0500

    minor changes in ui and responseOptions of converted schemas

--- tools/HBN2schema.js
+++ tools/HBN2schema.js
@@ -13,9 +13,13 @@ const schemaMap = {
     'Response Type': 'inputType',
     'Response Options': 'choices',
     'Stevens\' level of measurement': 'type',
-    // 'Position of \'0\'': 'Center',
-    // 'Continuous or Discrete': 'Discrete',
-}
+};
+const uiInputTypes = {
+    'single choice': 'radio',
+    'text entry': 'text',
+    'text entry.': 'text',
+    'numeric': 'number'
+};
 const uiList = ['inputType', 'shuffle'];
 const responseList = ['type', 'minValue', 'maxValue', 'requiredValue', 'multipleChoice'];
 const defaultLanguage = 'en';
@@ -117,7 +121,7 @@ csv
                         preambleObj[form][sectionName] = row['Question Group Instruction'];
                         // create section schema
                         createFormSchema(sectionName, formContextUrl, 0);
-                        contextOBj[sectionName] = { "@id": `https://raw.githubusercontent.com/ReproNim/schema-standardization/master/activities/${form}/${sectionName}_schema.jsonld` , "@type": "@id" };
+                        contextOBj[sectionName] = { "@id": `${form}/${sectionName}.jsonld` , "@type": "@id" };
                         if (order.indexOf(sectionName) === -1) {
                             order.push(sectionName);
                         }
@@ -156,13 +160,22 @@ function processRow(form, row){
     rowData[schemaMap['Question ID']] = field_name;
     Object.keys(row).forEach(current_key => {
 
-        // get schema key from mapping.json corresponding to current_key
         if (schemaMap.hasOwnProperty(current_key) && current_key !== 'Question ID') {
 
             // check all ui elements to be nested under 'ui' key
             if (uiList.indexOf(schemaMap[current_key]) > -1) {
-                let uiValue = row[current_key];
-
+                let uiValue = (row[current_key]).toLowerCase();
+                if (uiValue === 'single choice') {
+                    if (rowData.hasOwnProperty('responseOptions')) {
+                        rowData.responseOptions['multipleChoice'] = false;
+                    }
+                    else {
+                        rspObj['multipleChoice'] = false;
+                        rowData['responseOptions'] = rspObj;
+                    }
+                }
+                if (uiInputTypes.hasOwnProperty(uiValue))
+                    uiValue = uiInputTypes[uiValue];
                 if (rowData.hasOwnProperty('ui')) {
                     rowData.ui[schemaMap[current_key]] = uiValue;
                 }
