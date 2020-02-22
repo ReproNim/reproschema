832a6a88e604e31cdaf2536f000408c10cdfbbd6
Sanu Ann <sanuann@mit.edu>
2020-02-21 22:59:42 -0500

    script to parse files in proper json format

---
+++ validation/jsonParser.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+# invalid_json_files = []
+# read_json_files = []
+
+
+for root, dirs, files in os.walk('./activities/PHQ-9', topdown=True):
+    for name in files:
+        # files without extension or with .jsonld extn
+        if not os.path.splitext(name)[1] or os.path.splitext(name)[1] == '.jsonld':
+            # print(18, '--- ', name)
+            full_file_name = os.path.join(root, name)
+            with open(full_file_name) as json_file:
+                try:
+                    #print(25, json_file.read())
+                    #d = json.dumps(json_file)
+                    json.load(json_file)
+                    # read_json_files.append(files)
+                except ValueError as e:
+                    print ("File '%s' has JSON validation errors. %s" %(full_file_name, e))
+                    # invalid_json_files.append(files)
+
+# print(invalid_json_files, len(read_json_files))
