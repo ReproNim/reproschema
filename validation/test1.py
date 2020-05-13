bb03e1e88eb54bf1b654863f96596d98209e6705
Sanu Ann <sanuann@mit.edu>
2020-05-13 03:29:02 -0400

    update the shacl validator for activity schema

--- validation/test1.py
+++ validation/test1.py
@@ -1,23 +1,13 @@
 from pyld import jsonld
 from urllib.request import urlopen
+from pyshacl import validate
 import json
 import os
 
-url = 'https://raw.githubusercontent.com/sanuann/reproschema/master/activities/PHQ-9/PHQ9_schema'
+url = 'https://raw.githubusercontent.com/ReproNim/reproschema/master/activities/PHQ-9/PHQ9_schema'
 data = json.loads(urlopen(url).read().decode("utf-8"))
 
-# for root, dirs, files in os.walk('./activities/PHQ-9', topdown=True):
-#     for name in files:
-#         if name.endswith('_schema'):
-#             file_path = os.path.join(root, name)
-#             with open(file_path) as json_file:
-#                 try:
-#                     print(11, json.loads(file_path))
-#                 except ValueError as e:
-#                     print ("File '%s' has JSON validation errors. %s" %(file_path, e))
-#                     raise
-
-for root, dirs, files in os.walk('./activities/PHQ-9', topdown=True):
+for root, dirs, files in os.walk('./activities/PHQ-9'):
     for name in files:
         # files without extension or with .jsonld extn
         if name.endswith('_schema'):
@@ -28,6 +18,11 @@ for root, dirs, files in os.walk('./activities/PHQ-9', topdown=True):
                     #print(25, json_file.read())
                     #d = json.dumps(json_file)
                     data = json.load(json_file)
+                    normalized = jsonld.normalize(
+                        data, {'algorithm': 'URDNA2015', 'format':
+                            'application/n-quads'})
+                    r = validate(normalized, shacl_graph='validation/ActivityShape.ttl')
+                    conforms, results_graph, results_text = r
                     # read_json_files.append(files)
                 except ValueError as e:
                     print ("File '%s' has JSON validation errors. %s" %(full_file_name, e))
@@ -38,4 +33,4 @@ normalized = jsonld.normalize(
     data, {'algorithm': 'URDNA2015', 'format':
         'application/n-quads'})
 
-print(normalized)
+#print(normalized)
