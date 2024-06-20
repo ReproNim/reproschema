f0eb3b03c5459a03f9a89e8c2a25b45d438ebf92
Remi Gau <remi_gau@hotmail.com>
2024-06-20 16:00:36 +0200

    [DOC] render library listing in doc (#505)
    * [DATALAD] Added subdataset
    
    * lib in doc

---
+++ docs/library.md
@@ -0,0 +1,14 @@
+---
+hide:
+    - toc
+---
+# Reproschema library
+
+At the moment, all the assessments that support this standard are listed in [this folder](https://github.com/ReproNim/reproschema-library/tree/master/activities) of the [reproschema-library repository](https://github.com/ReproNim/reproschema-library).
+
+For convenience we are listing them in the table below.
+
+If you want to see those different tools in action using our user interface,
+you can explore them on [schema.repronim.org/](https://schema.repronim.org/rl/).
+
+{{ MACROS___library_table() }}
---
+++ macros/macros.py
@@ -0,0 +1,68 @@
+from pathlib import Path
+import json
+
+import ruamel.yaml
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+yaml = ruamel.yaml.YAML()
+yaml.indent(mapping=2, sequence=4, offset=2)
+
+ROOT = Path(__file__).parents[1]
+
+TEMPLATES_DIR = ROOT / "templates"
+
+LIBRARY_DIR = ROOT / "library"
+
+SCHEMA_DIR = ROOT / "linkml-schema"
+
+
+def return_jinja_env() -> Environment:
+    return Environment(
+        loader=FileSystemLoader(TEMPLATES_DIR),
+        autoescape=select_autoescape(),
+        lstrip_blocks=True,
+        trim_blocks=True,
+    )
+
+
+def library_table() -> str:
+
+    LIBRARY_URL = "https://github.com/ReproNim/reproschema-library"
+
+    activities = []
+
+    for activity_path in (LIBRARY_DIR / "activities").iterdir():
+
+        if not activity_path:
+            continue
+
+        for file in activity_path.glob("*"):
+
+            if file.is_dir() or file is None or "valueConstraints" in file.stem:
+                continue
+
+            with open(file) as f:
+                content = json.load(f)
+
+            activities.append(
+                {
+                    "name": content["@id"],
+                    "description": (
+                        content["description"] if "description" in content else ""
+                    ),
+                    "uri": f"{LIBRARY_URL}/tree/master/activities/{activity_path.stem}/{file.stem}{file.suffix}",
+                }
+            )
+
+    env = return_jinja_env()
+    template = env.get_template("library_table.jinja")
+
+    return template.render(activities=activities)
+
+
+def main():
+    print(library_table())
+
+
+if __name__ == "__main__":
+    main()
